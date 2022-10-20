d8c0a98ceac3103d838c34780c35e9cb69616c4f
Eliton Peroni <eliton_peroni@hotmail.com>
2022-10-20 16:51:27 -0300

    Adicionar arquivos de projeto.

---
+++ 06-bytebank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Eliton";
+            cliente.CPF = "123.31.123.122";
+            cliente.Profissao = "Desenvolvedor";
+                      
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+            conta.Depositar(100);
+            conta.Sacar(50);
+            
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(conta.Titular.CPF);
+            Console.ReadLine();
+
+
+        }
+    }
+}
